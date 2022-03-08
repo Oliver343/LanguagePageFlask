3217be26f833dd97537c4f7517889c7a261cdd3e
Oliver343 <93383676+Oliver343@users.noreply.github.com>
2022-03-08 17:38:49 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Language Page
+
+This is a webpage made to help none native English speakers learn *phrasal verbs* as it's something my students always have problems with.
+
+## Linked Data
+
+The file *questions_data.json* has the questions that are used in the quiz. More items can be added in at a later time without effecting the current code since the list length is used to choose a random question.
+
+The data is laid out in the following format:
+
+Key : Phrasal Verb, Example sentence 1, Example sentence 2, Answer 1, Answer 2.
+
+## Known issues
+
+* Since moving the correct answer to different radio buttons, it no longer displays correctly when the correct answer is selected.
+
+* Wrong answers are listed as such. I plan to have these populated with answers from other questions soon, so its a less obvious incorrect answer.
+
---
+++ app.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template, request, flash
+import json
+import random
+
+
+with open('questions_data.json') as f:
+    questions = json.load(f)
+    print("Loaded json data is: {}".format(questions))
+    # Retrieving data from json file
+
+app = Flask(__name__)
+app.secret_key = "sausage123"
+
+@app.route('/', methods=["POST", "GET"])
+def index():
+    question_number = random.randrange(len(questions))
+    correct_radio_button = random.randrange(0,3)
+    print("Correct answer is: ", correct_radio_button)
+    # The random numbers have to be generated here each time the page is loaded so they are different each time.
+    user_answer = request.form.get('radio_1')
+    print("Users selected answer is: ", user_answer)
+    # Gets the selected answer from radio buttons (returns None if none selected)
+
+
+    answer_1 = "Wrong answer 1"
+    answer_2 = "Wrong answer 2"
+    answer_3 = "Wrong answer 3"
+    # Incorrect answers are asigned to all options first
+    answer_list = [answer_1, answer_2, answer_3]
+    answer_list[correct_radio_button] = questions[str(question_number)][3]
+    # This sets the correct answer in the list using the random number generated before in 'correct_radio_button'
+    
+    if user_answer != None: # checks if an answer has been entered and if it is correct
+        if user_answer == str(correct_radio_button):
+            flash("Correct!")
+        else:
+            flash("Wrong answer")
+    return render_template('index.html', pass_question = questions[str(question_number)][0], 
+    pass_sentence = questions[str(question_number)][1], pass_answer1 = answer_list[0],
+    pass_answer2 = answer_list[1], pass_answer3 = answer_list[2])
+    # Variables are passed to index.html
+
+
+
+        
