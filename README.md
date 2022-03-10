ab0381740210d2a06b9ee2d37b40b45160f8062d
Oliver343 <93383676+Oliver343@users.noreply.github.com>
2022-03-10 19:30:21 +0100

    Correct answer now displays correctly when chosen

--- README.md
+++ README.md
@@ -13,6 +13,7 @@ Key : Phrasal Verb, Example sentence 1, Example sentence 2, Answer 1, Answer 2.
 ## Known issues
 
 * Since moving the correct answer to different radio buttons, it no longer displays correctly when the correct answer is selected.
+    This has now been resolved. The problem was that a new correct answer was generated when the page refreshed. I resolved this by storing the correct answer in a global variable however this means the function might not function as inteded if called in another part of the code making the overall code less robust however it does now function as inteded. 
 
 * Wrong answers are listed as such. I plan to have these populated with answers from other questions soon, so its a less obvious incorrect answer.
 
--- app.py
+++ app.py
@@ -2,17 +2,19 @@ from flask import Flask, render_template, request, flash
 import json
 import random
 
-
 with open('questions_data.json') as f:
     questions = json.load(f)
     print("Loaded json data is: {}".format(questions))
     # Retrieving data from json file
 
+carried_over = 0
+
 app = Flask(__name__)
-app.secret_key = "sausage123"
+app.secret_key = "sausage123" # Not used
 
 @app.route('/', methods=["POST", "GET"])
 def index():
+    global carried_over
     question_number = random.randrange(len(questions))
     correct_radio_button = random.randrange(0,3)
     print("Correct answer is: ", correct_radio_button)
@@ -20,8 +22,6 @@ def index():
     user_answer = request.form.get('radio_1')
     print("Users selected answer is: ", user_answer)
     # Gets the selected answer from radio buttons (returns None if none selected)
-
-
     answer_1 = "Wrong answer 1"
     answer_2 = "Wrong answer 2"
     answer_3 = "Wrong answer 3"
@@ -31,10 +31,11 @@ def index():
     # This sets the correct answer in the list using the random number generated before in 'correct_radio_button'
     
     if user_answer != None: # checks if an answer has been entered and if it is correct
-        if user_answer == str(correct_radio_button):
+        if user_answer == str(carried_over):
             flash("Correct!")
         else:
             flash("Wrong answer")
+    carried_over = correct_radio_button
     return render_template('index.html', pass_question = questions[str(question_number)][0], 
     pass_sentence = questions[str(question_number)][1], pass_answer1 = answer_list[0],
     pass_answer2 = answer_list[1], pass_answer3 = answer_list[2])
